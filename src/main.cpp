ee300a4b20341ad26b900f52022315f895663e8e
Stefan de Beer <beer.de.stefan@hotmail.com>
2021-07-18 21:16:27 +0200

    Can now find word in dict

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,58 @@
 #include <iostream>
 
 #include "SearchTree.h"
+#include <fstream>
+#include <chrono>
+
+using namespace std::chrono;
+
+void containsWord(DictSearchTree &dict, std::string_view word)
+{
+    if (dict.containsWord(word))
+    {
+        std::cout << "contains " << word << "\n";
+    }
+    else
+    {
+        std::cout << "Did not find " << word << "\n";
+    }
+}
 
 int main()
 {
-    DictSearchTree dict;
+    std::string sourceFileName = "words_alpha.txt";
+    std::fstream wordsFile;
 
-    if (dict.containsWord("test"))
+    wordsFile.open(sourceFileName);
+    if (wordsFile.is_open())
     {
-        std::cout << "Found word\n";
+        int wordCount = 0;
+        DictSearchTree dict;
+
+        auto begin = steady_clock::now();
+
+        std::string word;
+        while (std::getline(wordsFile, word))
+        {
+            dict.addWord(word);
+            wordCount++;
+        }
+
+        auto end = steady_clock::now();
+
+        std::cout << "Added " << wordCount << " words\n";
+        std::cout << "Creation took " << duration_cast<milliseconds>(end - begin).count() << " milliseconds\n";
+
+        containsWord(dict, "convict");
+        containsWord(dict, "cigarette");
+        containsWord(dict, "relaxation");
+        containsWord(dict, "miscarriage");
+        containsWord(dict, "calculation");
+        containsWord(dict, "jealous");
+        containsWord(dict, "ouijrfgzoruf");
     }
     else
     {
-        std::cout << "Did not find word\n";
+        std::cout << "Failed to open" << sourceFileName << std::endl;
     }
-
-    dict.addWord("test");
-
-    if (dict.containsWord("test"))
-    {
-        std::cout << "Found word\n";
-    }
-    else
-    {
-        std::cout << "Did not find word\n";
-    }
-
-    std::cout << "Hello world\n";
 }
